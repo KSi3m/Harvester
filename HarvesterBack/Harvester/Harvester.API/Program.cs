a442150c53ef2ac61e07040fa934e2c3045ceac9
ksi3m <mieszko9919@gmail.com>
2025-08-24 22:06:07 +0200

    change to cors policy

--- HarvesterBack/Harvester/Harvester.API/Program.cs
+++ HarvesterBack/Harvester/Harvester.API/Program.cs
@@ -28,7 +28,7 @@ builder.Services.AddCors(options =>
     {
         policy.WithOrigins("http://localhost:4200")
               .AllowAnyHeader()
-              .AllowAnyMethod();
+              .WithMethods("GET", "POST", "PUT", "DELETE", "PATCH");
     });
 });
 
