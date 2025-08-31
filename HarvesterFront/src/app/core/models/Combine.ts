9da8feb6ee7c260b64f8110bb34b7785ea8bc28b
ksi3m <mieszko9919@gmail.com>
2025-08-31 23:23:55 +0200

    initial work on add order form on frontend

---
+++ HarvesterFront/src/app/core/models/Combine.ts
@@ -0,0 +1,10 @@
+export interface Combine {
+  Id: number;
+  Model: string;
+  BaseHaPerHour: number;
+  HeaderLength: number;
+  IsAvailable: boolean;
+  CropType: string;
+  AvailableWorkHours: number;
+  BaseEfficency: number;
+}
