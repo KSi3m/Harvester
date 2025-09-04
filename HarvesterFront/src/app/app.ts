5a1fde24b18e0d3a06d73e3cfed3e0cdfd342db6
ksi3m <mieszko9919@gmail.com>
2025-09-04 22:58:24 +0200

    menubar added

--- HarvesterFront/src/app/app.ts
+++ HarvesterFront/src/app/app.ts
@@ -2,15 +2,45 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MessageService } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
+import { MenubarModule } from 'primeng/menubar';
 import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule, ToastModule],
+  imports: [RouterOutlet, ButtonModule, ToastModule, MenubarModule],
   templateUrl: './app.html',
   styleUrl: './app.scss',
   providers: [MessageService],
 })
 export class App {
   protected readonly title = signal('HarvesterFront');
+  items = [
+    {
+      label: 'Home',
+      icon: 'pi pi-home',
+      routerLink: '/',
+    },
+    {
+      label: 'Orders',
+      icon: 'pi pi-star',
+      //routerLink: '/',
+      items: [
+        {
+          label: 'Add new order',
+          icon: 'pi pi-plus',
+          routerLink: '/orders/add',
+        },
+      ],
+    },
+    /*{
+      label: 'Combines',
+      icon: 'pi pi-home',
+      routerLink: '/',
+    },*/
+    {
+      label: 'Add field',
+      icon: 'pi pi-expand',
+      routerLink: '/fields/add',
+    },
+  ];
 }
