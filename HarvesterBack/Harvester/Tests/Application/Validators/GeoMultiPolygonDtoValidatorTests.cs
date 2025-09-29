7cc4e60218a6a1cd6eafabb9fd0a73b46d25fac8
ksi3m <mieszko9919@gmail.com>
2025-09-29 21:49:05 +0200

    more validator tests

---
+++ HarvesterBack/Harvester/Tests/Application/Validators/GeoMultiPolygonDtoValidatorTests.cs
@@ -0,0 +1,58 @@
+﻿using FluentValidation.TestHelper;
+using Harvester.Application.Dtos;
+using Harvester.Application.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harvester.Tests.Application.Validators
+{
+    public class GeoMultiPolygonDtoValidatorTests
+    {
+        [Fact]
+        public void GeoMultiPolygonDtoValidator_ReturnsValidationErrors_WhenTypeIsInvalid()
+        {
+            var validator = new GeoMultiPolygonDtoValidator();
+            var dto = new GeoMultiPolygonDto { Type = "MMDDSFs", Coordinates = [] };
+
+            var result = validator.TestValidate(dto);
+
+            result.ShouldHaveValidationErrorFor(x => x.Type);
+        }
+
+        [Fact]
+        public void GeoMultiPolygonDtoValidator_ReturnsValidationErrors_WhenCoordinatesAreEmpty()
+        {
+            var validator = new GeoMultiPolygonDtoValidator();
+            var dto = new GeoMultiPolygonDto { Type = "MultiPolygon", Coordinates = [] };
+
+            var result = validator.TestValidate(dto);
+
+            result.ShouldHaveValidationErrorFor(x => x.Coordinates);
+        }
+
+        [Fact]
+        public void GeoMultiPolygonDtoValidator_DoesNotReturnValidationErrors_WhenDataIsValid()
+        {
+            var validator = new GeoMultiPolygonDtoValidator();
+            var dto = new GeoMultiPolygonDto { Type = "MultiPolygon", Coordinates = [
+                        [
+                            [
+                                [20.0, 50.0],
+                                [20.1, 50.0],
+                                [20.1, 50.1],
+                                [20.0, 50.1],
+                                [20.0, 50.0]
+                            ]
+                        ]
+                    ]
+            };
+
+            var result = validator.TestValidate(dto);
+
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+    }
+}
---
+++ HarvesterBack/Harvester/Tests/Application/Validators/GeoPointDtoValidatorTests.cs
@@ -0,0 +1,49 @@
+﻿using FluentValidation.TestHelper;
+using Harvester.Application.Dtos;
+using Harvester.Application.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harvester.Tests.Application.Validators
+{
+    public class GeoPointDtoValidatorTests
+    {
+        [Fact]
+        public void GeoPointDtoValidator_ReturnsValidationErrors_WhenTypeIsInvalid()
+        {
+            var validator = new GeoPointDtoValidator();
+            var dto = new GeoPointDto { Type = "Pointxx", Coordinates = [] };
+
+            var result = validator.TestValidate(dto);
+
+            result.ShouldHaveValidationErrorFor(x => x.Type);
+        }
+
+        [Theory]
+        [InlineData(new double[] { })]
+        [InlineData(new double[] {1,2,3,5 })]
+        public void GeoPointDtoValidator_ReturnsValidationErrors_WhenCoordinatesAreInvalid(double[] coords)
+        {
+            var validator = new GeoPointDtoValidator();
+            var dto = new GeoPointDto { Type = "Point", Coordinates = coords };
+
+            var result = validator.TestValidate(dto);
+
+            result.ShouldHaveValidationErrorFor(x => x.Coordinates);
+        }
+
+        [Fact]
+        public void GeoPointDtoValidator_DoesNotReturnValidationErrors_WhenDataIsValid()
+        {
+            var validator = new GeoPointDtoValidator();
+            var dto = new GeoPointDto { Type = "Point", Coordinates = [50.2,82.5] };
+
+            var result = validator.TestValidate(dto);
+
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+    }
+}
